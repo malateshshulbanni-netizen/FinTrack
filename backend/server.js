36faef9a1691870d06e4457013ab2beb96098e12
malatesh s hulbanni <mshulbanni95@gmail.com>
2026-09-11 10:54:36 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,8 @@ app.use(cors({
     'http://localhost:3000',
     'http://localhost:3001',
     'http://localhost:5173',
-    'https://fin-track-admin.vercel.app'
+    'https://fin-track-admin.vercel.app',
+    'https://fin-track-client-khaki.vercel.app'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
